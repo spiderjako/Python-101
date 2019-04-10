de8ac293c7a6ee423c3b527106a94f2f3f8e5659
root <root@localhost.localdomain>
2019-04-10 18:23:42 +0300

    new items for DaP

--- week7/dungeons_and_pyths/dungeons_and_pythons.py
+++ week7/dungeons_and_pyths/dungeons_and_pythons.py
@@ -1,6 +1,8 @@
 class Hero:
     __max_hp = 0
     __max_mana = 0
+    __attack_points = 0
+    __spell_points = 0
 
     def __init__(self, name, title, __health, __mana, __mana_regen):
         self.name = name
@@ -17,13 +19,10 @@ class Hero:
     def get_mana(self):
         return self.__mana
     def is_alive(self):
-        if self.get_hp() > 0:
-            return True
-        return False
+        return self.get_hp() > 0
+            
     def can_cast(self):
-        if self.get_mana() > 0:
-            return True
-        return False
+        return self.get_mana() > 0
 
     def take_damage(self, damage_points):
         if damage_points >= self.get_hp():
@@ -37,12 +36,20 @@ class Hero:
         elif self.__health == self.__max_hp:
             return 'Already at max'
         elif self.__health + healing_points < self.__max_hp:
-            
             self.__health += healing_points
             return self.__health
         else:
             self.__health = self.__max_hp
             return self.__health
+    def take_mana(self, mana_points):
+        if self.__mana == self.__max_mana:
+            return 'Already at max'
+        elif self.__mana + mana_points < self.__max_mana:
+            self.__mana += mana_points
+            return self.__mana
+        else:
+            self.__mana = self.__max_mana
+            return self.__mana
 
 h = Hero('Bron', 'Dragon', 100, 100, 2)
 
@@ -51,3 +58,4 @@ print(h.is_alive())
 print(h.can_cast())
 print(h.take_damage(50))
 print(h.take_healing(10))
+print(h.take)
